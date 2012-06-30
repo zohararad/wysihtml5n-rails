cb8ea67dcf20ddbdfa17c3630c267c7605f0d2c5
Zohar Arad <zohar@zohararad.com>
2012-06-30 22:10:06 +0300

    added tests for sprockets and individual scss file inclusion

--- test/cases/usage_css_spec.rb
+++ test/cases/usage_css_spec.rb
@@ -22,4 +22,36 @@ class UsageCssSpec < Wysihtml5n::Rails::Spec
 
   end
 
+  describe 'individual.scss' do
+
+    let(:individual_css) { dummy_asset('individual.css') }
+
+    it 'will render bootstrap, font-awesome and editor CSS' do
+      individual_css.must_include 'bootstrap.scss'
+      #individual_css.must_include 'editor.scss'
+      #individual_css.must_include 'font-awesome.scss'
+    end
+
+  end
+
+  describe 'sprockets.css' do
+
+    let(:sprockets_css) { dummy_asset('sprockets.css') }
+
+    it 'will render wysihtml5n file with all included dependencies' do
+      sprockets_css.must_include 'bootstrap.scss'
+      sprockets_css.must_include 'editor.scss'
+      sprockets_css.must_include 'font-awesome.scss'
+    end
+
+    it 'must include basic css afterward' do
+      sprockets_css.must_include '#other-css { color: red; }',  'From our code afterward.'
+    end
+
+    it 'must render the correct path for the icon sprites' do
+      sprockets_css.must_include 'url(/assets/fontawesome-webfont.woff)'
+    end
+
+  end
+
 end
