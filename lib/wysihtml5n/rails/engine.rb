1f415696eec863a3ee1bade91c1fc85cb6058b8a
Zohar Arad <zohar@zohararad.com>
2012-06-30 22:11:44 +0300

    added vendor fonts directory to asset pipeline path on gem init

--- lib/wysihtml5n/rails/engine.rb
+++ lib/wysihtml5n/rails/engine.rb
@@ -1,6 +1,9 @@
 module Wysihtml5n
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'wysihtml5n-rails.setup', :after => 'append_asset_paths', :group => :all do |app|
+        app.config.assets.paths << File.join(config.root, 'vendor', "assets", "fonts")
+      end
       # Make rails recognize vendored assets
     end
   end
